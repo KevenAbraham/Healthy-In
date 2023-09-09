a5305088cf2cfeb459dd41fc945210d6924ce3d2
KevenAbraham <abraham.keven20@gmail.com>
2023-09-09 10:53:16 -0300

    finalizando a sessão acima do footer

--- src/components/AboveFooter/AboveFooter.js
+++ src/components/AboveFooter/AboveFooter.js
@@ -4,9 +4,10 @@ import './AboveFooter.css';
 export default () => {
     return (
         <div className="aboveFooter">
-            <h3>Não perca tempo e junte-se conosco a conscientizar sobre uma vida saudável!</h3>
-
-            <button>Conheça</button>
+            <div className="aboveFooterContent">
+                <h3>Não perca tempo e junte-se conosco a conscientizar sobre uma vida saudável!</h3>
+                <button>Conheça</button>
+            </div>
         </div>
     )
 }

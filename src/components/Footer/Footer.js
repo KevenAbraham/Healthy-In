569dd98ec540a0ae860e6ac16908e558647e7f8c
KevenAbraham <abraham.keven20@gmail.com>
2023-10-03 19:17:25 -0300

    finalizando o footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -2,6 +2,12 @@ import React from "react";
 import './Footer.css';
 import Logo from '../../img/Logo.svg';
 import Linha from '../../img/linha.svg';
+import facebook from '../../img/1.svg';
+import instagram from '../../img/2.svg';
+import twitter from '../../img/3.svg';
+import pinterest from '../../img/4.svg';
+import youtube from '../../img/5.svg';
+import email from '../../img/6.svg';
 
 export default () => {
     return (
@@ -14,15 +20,15 @@ export default () => {
                 <li>Plante Legumes</li>
             </ul>
 
-            <img src={Linha} />
+            <img className="linha" src={Linha} />
 
             <ul className="socialMedia">
-                <li>Facebook</li>
-                <li>Instagram</li>
-                <li>Twitter</li>
-                <li>Pinterest</li>
-                <li>Youtube</li>
-                <li>Email</li>
+                <li><img src={facebook} /></li>
+                <li><img src={instagram} /></li>
+                <li><img src={twitter} /></li>
+                <li><img src={pinterest} /></li>
+                <li><img src={youtube} /></li>
+                <li><img src={email} /></li>
             </ul>
 
             <p>&copy; 2023 Projeto Healthy-In. Todos os direitos reservados.</p>

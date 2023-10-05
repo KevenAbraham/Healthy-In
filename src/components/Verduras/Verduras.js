95c452dd6a0e93852ca4542cf13820a1867a3ea4
KevenAbraham <abraham.keven20@gmail.com>
2023-10-05 18:40:52 -0300

    ajustando a posicao dos textos nos cards de verduras, como link, padding, posicao, tamanho

--- src/components/Verduras/Verduras.js
+++ src/components/Verduras/Verduras.js
@@ -11,7 +11,7 @@ export default () => {
                     <div className="boxText">
                         <h3 className="boxTitle">Alface</h3>
                         <p className="boxP">Folhas crocantes e refrescantes que podem ser a base perfeita para saladas saudáveis. Alface é uma hortense anual ou bienal, utilizada na alimentação humana desde cerca de 500 a.C..</p>
-                        <p className="link">Descubra mais</p>
+                        <p className="link"><span>Descubra mais</span></p>
                     </div>
                 </div>
                 
@@ -19,7 +19,7 @@ export default () => {
                     <div className="boxText">
                         <h3 className="boxTitle">Cebolinha</h3>
                         <p className="boxP">Uma das hortaliças mais utilizadas e comercializadas em todo o mundo, as cebolinhas frescas dão um toque de sabor especial e suave a diversos pratos de nossas rotinas diárias.</p>
-                        <p className="link">Descubra mais</p>
+                        <p className="link"><span>Descubra mais</span></p>
                     </div>
                 </div>
                 
@@ -27,7 +27,7 @@ export default () => {
                     <div className="boxText">
                         <h3 className="boxTitle">Espinafre</h3>
                         <p className="boxP">O espinafre é uma verdura caracterizada por suas folhas verdes escuras e é rico em nutrientes, sendo uma excelente fonte de vitaminas e minerais essenciais, como as vitaminas K, A, C, ácido fólico, ferro e cálcio.</p>
-                        <p className="link">Descubra mais</p>
+                        <p className="link"><span>Descubra mais</span></p>
                     </div>
                 </div>
                 
@@ -35,7 +35,7 @@ export default () => {
                     <div className="boxText">
                         <h3 className="boxTitle">Couve</h3>
                         <p className="boxP">A couve é uma verdura crucífera, com folhas verdes e rica em nutrientes, além de ser versátil podendo ser usada em pratos quentes e frios.</p>
-                        <p className="link">Descubra mais</p>
+                        <p className="link"><span>Descubra mais</span></p>
                     </div>
                 </div>
                 
@@ -43,7 +43,7 @@ export default () => {
                     <div className="boxText">
                         <h3 className="boxTitle">Rúcula</h3>
                         <p className="boxP">Uma verdura da mesma família que a mostarda, Brassicaceae, originária do Mediterrâneo e da Ásia Ocidental, a rúcula adiciona um toque picante e fresco às saladas.</p>
-                        <p className="link">Descubra mais</p>
+                        <p className="link"><span>Descubra mais</span></p>
                     </div>
                 </div>
                 
@@ -51,7 +51,7 @@ export default () => {
                     <div className="boxText">
                         <h3 className="boxTitle">Acelga</h3>
                         <p className="boxP">A acelga, também conhecida como beterraba branca, é uma hortaliça de talos longos e firmes, as folhas podem ser opacas ou brilhantes na coloração verde ou avermelhada.</p>
-                        <p className="link">Descubra mais</p>
+                        <p className="link"><span>Descubra mais</span></p>
                     </div>
                 </div>
                 
